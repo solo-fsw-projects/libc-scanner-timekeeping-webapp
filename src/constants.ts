ce3d32476b57af5d46e97f871ae6cec2fda67673
ElioS-S <37584025+ElioS-S@users.noreply.github.com>
2026-01-22 14:23:51 +0100

    Remove 'R' and 'C' from default unbillable project codes

--- src/constants.ts
+++ src/constants.ts
@@ -13,7 +13,7 @@ export const LATE_CANCELLATION_DAYS = 7
 export const UNKNOWN_PROJECT_LABEL = 'UNKNOWN'
 
 // Default project codes that should start as unbillable:
-export const DEFAULT_UNBILLABLE_PROJECT_CODES = [UNKNOWN_PROJECT_LABEL.toUpperCase(), 'Z', 'R', 'C']
+export const DEFAULT_UNBILLABLE_PROJECT_CODES = [UNKNOWN_PROJECT_LABEL.toUpperCase(), 'Z']
 
 // Words indicating cancellation in various languages:
 export const CANCEL_WORDS = [
